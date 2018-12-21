72ac62105c97bd4a1326ba79c97249586df28a48
DrSnail <guardfeasul@gmail.com>
2018-12-21 19:56:05 +0700

    Добавил список верных команд. Добавил обработку ошибок

--- Final_task/Calculator_1/Calc_Classes/ERRORS.py
+++ Final_task/Calculator_1/Calc_Classes/ERRORS.py
@@ -1,3 +1,5 @@
+valid_commands = ["ADD", "SUB", "MUL", "DIV", "SET", "PRINT"]
+
 class ResultReturnError(Exception):
     def __str__(self):
         return "Result is equal None"
@@ -5,3 +7,15 @@ class ResultReturnError(Exception):
 class MultipleVariablesError(Exception):
     def __str__(self):
         return ""
+
+class InputError(Exception):
+    def __str__(self):
+        return "Не верная комманда"
+
+class CommandInputError(InputError):
+    def __str__(self):
+        return "Такой комманды не существует"
+
+class VariableInputError(InputError):
+    def __str__(self):
+        return "Не вернно введены переменные"
--- Final_task/Calculator_1/main.py
+++ Final_task/Calculator_1/main.py
@@ -1,13 +1,31 @@
-from .Calc_Classes.Operators import *
-from .Calc_Classes.Variables import *
+from Final_task.Calculator_1.Calc_Classes.ERRORS import *
+from Final_task.Calculator_1.Calc_Classes.Variables import *
+from Final_task.Calculator_1.Calc_Classes.Operators import *
+import logging
+
+dynamic_input_dic = {}
+dynamic_user_variables_dic = {}
+var1 = None
+var2 = None
+log = logging.getLogger("Errors")
 
 if __name__ == "__main__":
-    var1 = Variable("my_name", 10)
-    v2 = 2
+    while True:
+        custom_input = input("$:")
+        # Разбить строку на команды
+        custom_input = custom_input.split()
+        # Если пользователь ввел 'q', то завершить исполнение программы
+        if custom_input[0] == "q":
+            break
 
-    custom_input = ADD(var1, v2)
-    custom_input.calculate()
-    custom_input.get_result(show=True)
+        try:
+            if len(custom_input) < 3 or len(custom_input) > 3:
+                raise InputError
+            elif custom_input[0] not in valid_commands:
+                raise CommandInputError
+        except InputError as err:
+            log.warning(err.__str__())
+            continue
 
-    print(var1.var_value)
-    pr
+        if custom_input[0] == "ADD":
+            pass
