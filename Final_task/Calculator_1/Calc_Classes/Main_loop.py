e8d868bcfe9af8a021d232619cddf9c0c3038da8
DrSnail <guardfeasul@gmail.com>
2018-12-22 13:51:46 +0700

    Добавлен метод для проверки есть ли в пользовательском вводе комманда PRINT

--- Final_task/Calculator_1/Calc_Classes/Main_loop.py
+++ Final_task/Calculator_1/Calc_Classes/Main_loop.py
@@ -6,6 +6,8 @@ class Iterator():
 class User_input():
     def __init__(self, user_input: str):
         self.user_input = user_input.split()
+        self.__containt_PRINT_command = None
+        self.__user_input_contain_print()
 
     def make_a_command(self, dynamic_user_variables_dic):
         """
@@ -32,3 +34,9 @@ class User_input():
                         self.user_input[i] = dynamic_user_variables_dic[self.user_input[i]]
                     else:
                         raise VariablesTypeError
+
+    def __user_input_contain_print(self):
+        if "." in self.user_input[0] or "," in self.user_input[0]:
+            self.__containt_PRINT_command = True
+        else:
+            self.__containt_PRINT_command = False
