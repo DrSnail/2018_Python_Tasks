a97c3b564f81f8b8386592a3b07146c697b50693
DrSnail <guardfeasul@gmail.com>
2018-12-22 13:44:20 +0700

    Класс User_Input и Iterator добавлен. Предназначен для работы с пользовательскими коммандами

---
+++ Final_task/Calculator_1/Calc_Classes/Main_loop.py
@@ -0,0 +1,34 @@
+from Final_task.Calculator_1.Calc_Classes.ERRORS import *
+
+class Iterator():
+    pass
+
+class User_input():
+    def __init__(self, user_input: str):
+        self.user_input = user_input.split()
+
+    def make_a_command(self, dynamic_user_variables_dic):
+        """
+        Конвертирует пользовательский ввод в int
+        :param custom_input: строка, которую нужно конвертировать
+        :type custom_input: int, float, Variable
+        :return:
+        :rtype:
+        """
+        for i in range(1, len(self.user_input)):
+            if "." in self.user_input[i] or "," in self.user_input[i]:
+                try:
+                    self.user_input[i] = float(self.user_input[i])
+                except ValueError:
+                    if self.user_input[i] in dynamic_user_variables_dic:
+                        self.user_input[i] = dynamic_user_variables_dic[self.user_input[i]]
+                    else:
+                        raise VariablesTypeError
+            else:
+                try:
+                    self.user_input[i] = int(self.user_input[i])
+                except ValueError:
+                    if self.user_input[i] in dynamic_user_variables_dic:
+                        self.user_input[i] = dynamic_user_variables_dic[self.user_input[i]]
+                    else:
+                        raise VariablesTypeError
